9ba51942ade78edf355853bd8facb65933c6e481
NAM NGUYEN <Nam.Nguyen@ibm.com>
2021-10-26 11:32:45 -0400

    improve for Daniel stuffs

--- modal.js
+++ modal.js
@@ -1,7 +1,46 @@
 class Modal extends HTMLElement {
   constructor() {
     super();
+    this._title = 'Default Title';
+    this._message =
+      'This is the default message show when displaying the modal';
     this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('title')) this._title = this.getAttribute('title');
+    if (this.hasAttribute('message'))
+      this._message = this.getAttribute('message');
+    this.render();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // switch (name) {
+    //   case 'opened': {
+    //     if (this.hasAttribute('opened')) {
+    //       this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+    //       this.shadowRoot.querySelector('#backdrop').style.pointerEvents =
+    //         'all';
+    //       this.shadowRoot.querySelector('#modal').style.opacity = 1;
+    //       this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+    //     }
+    //   }
+    // }
+  }
+
+  static get observedAttributes() {
+    return ['opened'];
+  }
+
+  open() {
+    this.setAttribute('opened', '');
+  }
+
+  close() {
+    this.removeAttribute('opened');
+  }
+
+  render() {
     this.shadowRoot.innerHTML = `
       <style>
         #backdrop {
@@ -66,43 +105,32 @@ class Modal extends HTMLElement {
           margin: 0 0.25rem;
         }
       </style>
-      <div id="backdrop">
-        <div id="modal">
-          <header>
-            <slot name="title"><h1>Default Header</h1></slot>
-          </header>
-          <section id="main">
-            <slot></slot>
-          </section>
-          <section id="actions">
-            <button>Cancel</button>
-            <button>Confirm</button>
-          </section>
+      <div>
+        <p>Please confirm your choice</p>
+        <button id="triggerButton">Show detail & confirm</button>
+        <div id="backdrop">
+          <div id="modal">
+            <header>
+              <slot name="title"><h1>${this._title}</h1></slot>
+            </header>
+            <section id="main">
+              <slot name="message">${this._message}</slot>
+            </section>
+            <section id="actions">
+              <button id="cancelButton">Cancel</button>
+              <button id="confirmButton">Confirm</button>
+            </section>
+          </div>
         </div>
       </div>
     `;
-  }
 
-  attributeChangedCallback(name, oldValue, newValue) {
-    // switch (name) {
-    //   case 'opened': {
-    //     if (this.hasAttribute('opened')) {
-    //       this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-    //       this.shadowRoot.querySelector('#backdrop').style.pointerEvents =
-    //         'all';
-    //       this.shadowRoot.querySelector('#modal').style.opacity = 1;
-    //       this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-    //     }
-    //   }
-    // }
-  }
-
-  static get observedAttributes() {
-    return ['opened'];
-  }
-
-  open() {
-    this.setAttribute('opened', '');
+    let cancelButton = this.shadowRoot.querySelector('#cancelButton');
+    cancelButton.addEventListener('click', this.close.bind(this));
+    let confirmButton = this.shadowRoot.querySelector('#confirmButton');
+    confirmButton.addEventListener('click', this.close.bind(this));
+    let triggerButton = this.shadowRoot.querySelector('#triggerButton');
+    triggerButton.addEventListener('click', this.open.bind(this));
   }
 }
 
